27273a6c626f836fd1114b50c9fb69b0e265a8ba
James Ngo <jamesngo@Jamess-MacBook-Pro-2.local>
2019-11-13 09:46:13 -0800

    Added visualization of 4 layers

--- main.py
+++ main.py
@@ -129,22 +129,24 @@ print("Predicted class is:", classes)
 
 # Try to visualize the CNN models
 # Extracts the outputs of the top 12 layers
-layer_outputs = [layer.output for layer in model.layers[:12]]
+layer_outputs = [layer.output for layer in model.layers[:4]]
 # Creates a model that will return these outputs, given the model input
 activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
 # Returns a list of five Numpy arrays: one array per layer activation
 activations = activation_model.predict(img_tensor)
 
 layer_names = []
-for layer in model.layers[:12]:
+for layer in model.layers[:4]:
     layer_names.append(layer.name)  # Names of the layers, so you can have them as part of your plot
 
-images_per_row = 16
+images_per_row = 8
 
 for layer_name, layer_activation in zip(layer_names, activations):  # Displays the feature maps
+    # print(layer_activation.shape)
     n_features = layer_activation.shape[-1]  # Number of features in the feature map
     size = layer_activation.shape[1]  # The feature map has shape (1, size, size, n_features).
     n_cols = n_features // images_per_row  # Tiles the activation channels in this matrix
+    # print("size: %d, n_cols: %d, images_per_row: %d" % (size, n_cols, images_per_row))
     display_grid = np.zeros((size * n_cols, images_per_row * size))
     for col in range(n_cols):  # Tiles each filter into a big horizontal grid
         for row in range(images_per_row):
@@ -161,6 +163,10 @@ for layer_name, layer_activation in zip(layer_names, activations):  # Displays t
     scale = 1. / size
     plt.figure(figsize=(scale * display_grid.shape[1],
                         scale * display_grid.shape[0]))
+    # print(display_grid.shape)
+    # print(scale*display_grid.shape[1])
+    # print(scale*display_grid.shape[0])
     plt.title(layer_name)
     plt.grid(False)
     plt.imshow(display_grid, aspect='auto', cmap='viridis')
+plt.show()

82153b5f1977b98036540d7c354bf36c8259f573
Darryl Yeo <dev@darryl-yeo.com>
2019-11-14 17:33:12 -0800

    Light refactoring; trying to figure out what's happening

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ GENERATE_NEW_MODEL = False
 NUM_EPOCHS = 10
 BATCH_SIZE = 200
 
+IMAGES_PER_ROW = 8
+
 if GENERATE_NEW_MODEL:
     model, model_gen = generate_model(NUM_EPOCHS, BATCH_SIZE)
     plot_fit_performance(model_gen)
@@ -48,22 +50,17 @@ activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
 # Returns a list of five Numpy arrays: one array per layer activation
 activations = activation_model.predict(img_tensor)
 
-layer_names = []
-for layer in model.layers[:4]:
-    layer_names.append(layer.name)  # Names of the layers, so you can have them as part of your plot
-
-images_per_row = 8
-
-for layer_name, layer_activation in zip(layer_names, activations):  # Displays the feature maps
-    # print(layer_activation.shape)
-    n_features = layer_activation.shape[-1]  # Number of features in the feature map
-    size = layer_activation.shape[1]  # The feature map has shape (1, size, size, n_features).
-    n_cols = n_features // images_per_row  # Tiles the activation channels in this matrix
-    # print("size: %d, n_cols: %d, images_per_row: %d" % (size, n_cols, images_per_row))
-    display_grid = np.zeros((size * n_cols, images_per_row * size))
+# Display a feature map for each layer in the neural network
+for layer, activation in zip(model.layers[:4], activations):
+    # print(activation.shape)
+    n_features = activation.shape[-1]  # Number of features in the feature map
+    size = activation.shape[1]  # The feature map has shape (1, size, size, n_features).
+    n_cols = n_features // IMAGES_PER_ROW  # Tiles the activation channels in this matrix
+    # print("size: %d, n_cols: %d, IMAGES_PER_ROW: %d" % (size, n_cols, IMAGES_PER_ROW))
+    display_grid = np.zeros((size * n_cols, IMAGES_PER_ROW * size))
     for col in range(n_cols):  # Tiles each filter into a big horizontal grid
-        for row in range(images_per_row):
-            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+        for row in range(IMAGES_PER_ROW):
+            channel_image = activation[0, :, :, col * IMAGES_PER_ROW + row]
             channel_image -= channel_image.mean()  # Post-processes the feature to make it visually palatable
             channel_image /= channel_image.std()
             channel_image *= 64
@@ -72,7 +69,7 @@ for layer_name, layer_activation in zip(layer_names, activations):  # Displays t
             display_grid[col * size: (col + 1) * size, row * size: (row + 1) * size] = channel_image
     scale = 1. / size
     plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
-    plt.title(layer_name)
+    plt.title(layer.name)
     plt.grid(False)
     plt.imshow(display_grid, aspect='auto', cmap='viridis')
 plt.show()
